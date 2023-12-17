1740293a1c4b0029b371d1df95690610ca0c1928
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-12-17 21:51:48 +0545

    Modification:Header.jsx,style.css

--- src/components/Jooneli/Header/Header.jsx
+++ src/components/Jooneli/Header/Header.jsx
@@ -1,29 +1,64 @@
-import React from "react";
-import Nav from "../nav-items/Nav";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./header.css";
+// import Home from "./home";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
   return (
     <>
-      <header className="header">
-        {/* <video
-          src="/video/video.mp4"
-          autoPlay
-          muted
-          className="animator"
-        ></video> */}
-        <img src="/image/background.jpeg" alt="" />
-        <div className="hover-effect--background "></div>
-        <div className="center-content "></div>
-        <div className="container">
-          <div className="logo-container">
-            <figure>
-              <Link to="/">
-                <img src="/image/jooneli.png" alt="" />
-              </Link>
-            </figure>
+      <header className="header-section">
+        <div className="nav-container">
+          <div className="logo-section">
+            <Link to="/">
+              <img src="/image/jooneli.png" alt="Brand Logo" />
+            </Link>
           </div>
-          <Nav />
+          <div className={` ${ show ? "hover-effect--background" : "none"}`}></div>
+          <div className="nav-bars">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+            <div
+              className="hover-effectors boxes"
+              onMouseOver={() => setShow(true)}
+              onMouseLeave={() => setShow(false)}
+            >
+              <div
+                className="display-product"
+                // onMouseOver={() => setShow(true)}
+                // onMouseLeave={() => setShow(false)}
+              >
+                Product
+                <div className="drop-down--product">
+                  <Link to="/home">360 Core Banking System</Link>
+                  <Link to="/home">Jooneli Trading</Link>
+                </div>
+              </div>
+              <div
+                className="display-company"
+                // onMouseOver={() => setShow(true)}
+                // onMouseLeave={() => setShow(false)}
+              >
+                Company
+                <div className="drop-down--company">
+                  <Link to="/">360 Core Banking System</Link>
+                  <Link to="/">Banking System</Link>
+                </div>
+              </div>
+            </div>
+            <ul>
+              <li>
+                <Link to="/">Blog</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="center-content">
+          <img src="/image/background.jpeg" alt="Background Content" />
+          We are Human
         </div>
       </header>
     </>
