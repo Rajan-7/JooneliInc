b87dd9d424331e6b1d74d95541d17cbd2bfb8ae3
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-04-13 19:35:01 +0545

    ft:modification AdminBlogs.jsx

--- src/components/Jooneli/Admin/AdminBlogs.jsx
+++ src/components/Jooneli/Admin/AdminBlogs.jsx
@@ -1,17 +1,75 @@
-import React from "react";
-import "./Admin.css"
+import React, { useState } from "react";
+import axios from "axios";
+import "./Admin.css";
+import { useAuth } from "../store/auth";
+
+const IURL = "http://localhost:5005/api/image/images";
+const BURL = "http://localhost:5005/api/admin/blogs";
 
 const AdminBlogs = () => {
+  const [blogs, setBlogs] = useState({
+    name: "",
+    description: "",
+    image: "",
+  });
+  const [img, setImg] = useState([]);
+  const { authorizationToken } = useAuth();
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setBlogs({
+      ...blogs,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formdata = new FormData();
+      formdata.append("image", img);
+
+      const imgResponse = await axios({
+        url: IURL,
+        data: formdata,
+        method: "post",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      const response = await axios.post(
+        BURL,
+        {
+          name: blogs.name,
+          description: blogs.description,
+          image: imgResponse.data.url,
+        },
+        {
+          headers: {
+            Authorization: authorizationToken,
+          },
+        }
+      );
+      console.log(response);
+      setBlogs({ name: "", description: "", image: "" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <section className="admin-user-section">
+    <section className="admin-user-section b-section">
       <section className="admin-user-section">
         <div className="container heading">
           <h1>Blogs Form</h1>
         </div>
         <div className="container grid grid-two-cols">
           {/* Update form */}
-          <section className="form-section">
-            <form>
+          <section className="form-section  blog-form">
+            <form onSubmit={handleSubmit}>
               <div>
                 <label htmlFor="image">Image</label>
                 <input
@@ -19,7 +77,10 @@ const AdminBlogs = () => {
                   name="image"
                   id="image"
                   required
+                  accept="image"
                   autoComplete="off"
+                  placeholder="Choose your image"
+                  onChange={(e) => setImg(e.target.files[0])}
                 />
               </div>
               <div>
@@ -30,18 +91,24 @@ const AdminBlogs = () => {
                   id="name"
                   required
                   autoComplete="off"
+                  placeholder="Name of the blogs..."
+                  value={blogs.name}
+                  onChange={handleChange}
                 />
               </div>
               <div>
-                <label htmlFor="message">Description</label>
+                <label htmlFor="description">Description</label>
                 <textarea
-                  name="message"
-                  id="message"
+                  name="description"
+                  id="description"
+                  placeholder="Description..."
                   cols="25"
                   rows="5"
+                  value={blogs.description}
+                  onChange={handleChange}
                 ></textarea>
               </div>
-              <div>
+              <div className="b_btn">
                 <button type="submit">Submit</button>
               </div>
             </form>
